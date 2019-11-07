da05d6a69d671bbce8812644d7e9cefb876fce30
Jacques Cornat <cornat.jacques@gmail.com>
2019-11-07 13:02:14 +0100

    fix boot && update result

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 const app = express();
@@ -6,15 +5,19 @@ const app = express();
 app.get('/', function (req, res) {
   const xForwardedFor = req.headers['x-forwarded-for'];
 
-  let connectionRemoteAddress = req.connection.remoteAddress;
-  if (connectionRemoteAddress) {
+  let connectionRemoteAddress;
+  const originalConnectionRemoteAddress = req.connection.remoteAddress;
+  if (originalConnectionRemoteAddress) {
     const regexp = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
-    connectionRemoteAddress = regexp.exec(connectionRemoteAddress);
+    const regexpResult = regexp.exec(originalConnectionRemoteAddress);
+    if (regexpResult) {
+      connectionRemoteAddress = regexpResult[0];
+    }
   }
 
   // const socketRemoveAddress = req.socket.remoteAddress;
 
-  var ip = xForwardedFor || connectionRemoteAddress;
+  const ip = xForwardedFor || connectionRemoteAddress;
   res.send(ip);
 });
 
